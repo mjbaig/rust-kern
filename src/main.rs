1448e09027fd4b74ad3d9cd0a8f32edff4233963
Maz Baig <7367135+mjbaig@users.noreply.github.com>
2024-03-04 10:25:02 -0800

    remove image from git

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{arch::asm, panic::PanicInfo};
 
 mod boot {
     use core::arch::global_asm;
@@ -11,7 +11,31 @@ mod boot {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    loop {
+        unsafe {
+            // pin 21 turned into output
+            let addresss: u32 = 0x3F20_0008;
+            core::ptr::write_volatile(addresss as *mut u32, 1 << 3);
+
+            loop {
+                // set pin 21 to high
+                let high_address: u32 = 0x3F20_001C;
+                core::ptr::write_volatile(high_address as *mut u32, 1 << 21);
+
+                for _ in 0..50000 {
+                    asm!("nop");
+                }
+
+                // set pin 21 to low
+                let low_address: u32 = 0x3F20_0028;
+                core::ptr::write_volatile(low_address as *mut u32, 1 << 21);
+
+                for _ in 0..50000 {
+                    asm!("nop");
+                }
+            }
+        }
+    }
 }
 
 #[panic_handler]
